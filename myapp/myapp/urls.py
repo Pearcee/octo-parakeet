1b6a6e40f03de8b7b104b779069a0fff04223415
steve pearce <sjpearce@gmail.com>
2022-03-04 14:07:24 +0000

    url

--- myapp/myapp/urls.py
+++ myapp/myapp/urls.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
 from django.urls import include, path
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-from django.conf.urls  import  include, url
 
 
 urlpatterns = [
     
     path('admin/', admin.site.urls),
-    url(r'^todo/', include('todo.urls')),
-    url(r'^blog/', include('blog.urls')),
+    path('todo/', include('todo.urls')),
+    path('blog/', include('blog.urls')),
     path("", include("index.urls")),
 ]
 
